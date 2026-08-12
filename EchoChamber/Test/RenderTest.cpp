6d964b9f3121e9b769b6bbe04a4d0ae938c44074
tiffani7577 <tiffanileblanc@rocketmail.com>
2026-08-12 03:08:41 -0400

    feat(EchoChamber): archive Test/RenderTest.cpp from Foundry

--- EchoChamber/Test/RenderTest.cpp
+++ EchoChamber/Test/RenderTest.cpp
@@ -174,7 +174,7 @@ int main()
     }
     {
         // Zipper / automation step-response energy on first continuous float param
-        const char* stepId = "mix";
+        const char* stepId = "roomSize";
         double clickEnergy = 0.0;
         if (std::strlen (stepId) > 0)
         {
